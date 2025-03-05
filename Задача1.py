e12d048636ebfecc3f564f8b2ce2982c1152414b
Annushka-k <Kas1012ov@yandex.ru>
2025-03-05 16:05:22 +0300

    Add files via upload

--- Задача1.py
+++ Задача1.py
@@ -2,6 +2,13 @@ a=int(input())
 b=int(input())
 c=int(input())
 d=int(input())
-print(' ', c, d)
+string=''
+for k in range(c,d+1):
+    string+=str(k)+' '
+print(' ', string)
 for i in range(a, b+1):
-        print(i, i*c, i*d)
+    print(i, ')', end='', sep='')
+    for j in range(c, d+1):
+        print(i*j, end=' ')
+    print()
+
